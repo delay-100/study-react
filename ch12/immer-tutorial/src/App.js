59d370a47a133e4879faa5885e141fe7d66df921
delay100 <qor3127@naver.com>
2022-07-22 15:08:31 +0900

    immer end..~

--- ch12/immer-tutorial/src/App.js
+++ ch12/immer-tutorial/src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useCallback, useState } from "react";
+import produce from "immer";
 
-function App() {
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: "", username: "" });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+
+  // input 수정을 위한 함수
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+
+      // immer 적용
+      setForm(
+        // produce(form, (draft) => {
+        // useState의 함수형 업데이트와 immer 함께 쓰기
+        produce((draft) => {
+          draft[name] = value;
+        })
+      );
+
+      // // 기존 코드
+      // setForm({
+      //   ...form,
+      //   [name]: [value],
+      // });
+    },
+    []
+    // [form]
+  );
+
+  // form 등록을 위한 함수
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+      // array에 새 항목 등록
+      // immer 적용
+      setData(
+        // produce(data, (draft) => {
+        // useState의 함수형 업데이트와 immer 함께 쓰기
+        produce((draft) => {
+          draft.array.push(info); // immer를 사용하여 컴포넌트 상태를 작성 시, 객체 안에 있는 값을 직접 수정하거나 배열에 직접적인 변화를 일으키는 push, splice 등의 함수를 사용해도 무방함
+        })
+      );
+      // 기존 코드
+      // setData({
+      //   ...data,
+      //   array: data.array.concat(info),
+      // });
+
+      // form 초기화
+      setForm({
+        name: "",
+        username: "",
+      });
+      nextId.current += 1;
+    },
+    [form.name, form.username]
+    // [data, form.name, form.username]
+  );
+
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback(
+    (id) => {
+      // immer 적용
+      setData(
+        // produce(data, (draft) => {
+        // useState의 함수형 업데이트와 immer 함께 쓰기
+        produce((draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1
+          );
+        })
+      );
+      // 기존 코드
+      // setData({
+      //   ...data,
+      //   array: data.array.filter((info) => info.id !== id),
+      // });
+    },
+    []
+    // [data]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="아이디"
+          value={form.username}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="이름"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username}({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
