230773f0f277fece6fa4dd754d5c47104ea477c7
delay100 <qor3127@naver.com>
2022-07-25 15:39:26 +0900

    useNaviate를 추가했고.. NavLink가 왜 안먹을까요..?

--- ch13/router-tutorial/src/Layout.js
+++ ch13/router-tutorial/src/Layout.js
@@ -1,10 +1,25 @@
 // 공통 레이아웃을 위한 Layout 컴포넌트
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    // 이전 페이지로 이동
+    navigate(-1); // navigate(-1)을 하면 뒤로 한번 감, 파라미터에 -2를 넣으면 뒤로 두 번감, 1을 넣으면 앞으로 한 번가는데, 뒤로가기를 한 번 한 상태여야 함
+  };
+
+  const goArticles = () => {
+    // articles 경로로 이동
+    // navigate("/articles");
+
+    navigate("/articles", { replace: true }); // replace 옵션: 페이지를 이동할 때 현재 페이지를 페이지 기록에 남기지 않음
+  };
+
   return (
     <div>
       <header style={{ background: "lightgray", padding: 16, fontSize: 24 }}>
-        Header
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goArticles}>게시글 목록</button>
       </header>
       <main>
         <Outlet />
--- ch13/router-tutorial/src/pages/Articles.js
+++ ch13/router-tutorial/src/pages/Articles.js
@@ -1,20 +1,43 @@
 // import { Link } from "react-router-dom"; // 중첩된 라우트 사용 전 기존 방식 예시
-import { Link, Outlet } from "react-router-dom"; // 중첩된 라우트 사용 예시
+// import { Link, Outlet } from "react-router-dom"; // 중첩된 라우트 사용 예시
+import { NavLink, Outlet } from "react-router-dom"; // NavLink 컴포넌트
 
 const Articles = () => {
+  const activateStyle = {
+    color: "green",
+    fontSize: 21,
+  };
   return (
     <div>
       <Outlet />
       {/* Outlet 컴포넌트가 사용된 자리에 중첩된 라우트가 보여지게 됩니다. */}
       <ul>
         <li>
+          {/*
           <Link to="/articles/1">게시글 1</Link>
+        */}
+          <NavLink
+            to="/articles/1"
+            style={({ isActivate }) => (isActivate ? activateStyle : undefined)}
+          >
+            게시글 1
+          </NavLink>
         </li>
         <li>
-          <Link to="/articles/2">게시글 2</Link>
+          <NavLink
+            to="/articles/2"
+            style={({ isActivate }) => (isActivate ? activateStyle : undefined)}
+          >
+            게시글 2
+          </NavLink>
         </li>
         <li>
-          <Link to="/articles/3">게시글 3</Link>
+          <NavLink
+            to="/articles/3"
+            style={({ isActivate }) => (isActivate ? activateStyle : undefined)}
+          >
+            게시글 3
+          </NavLink>
         </li>
       </ul>
     </div>
