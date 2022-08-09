3cc041b2621d8c1844f739a047cd749f2bc6ba24
delay100 <qor3127@naver.com>
2022-08-09 23:11:02 +0900

    ....

--- ch17/react-redux-tutorial/src/App.js
+++ ch17/react-redux-tutorial/src/App.js
@@ -1,10 +1,12 @@
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
   return (
     <div>
-      <Counter number={0} />
+      {/* <Counter number={0} /> */}
+      <CounterContainer /> {/* Counter을 CounterContainer로 교체 */}
       <hr />
       <Todos />
     </div>
--- ch17/react-redux-tutorial/src/index.js
+++ ch17/react-redux-tutorial/src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules';
-const store = createStore(rootReducer);
-// const store = createStore(rootReducer, composeWithDevTools());
+
+// 리액트 애플리케이션에 리덕스 적용하기
+// 1. 스토어 생성하기
+// const store = createStore(rootReducer);
+// 3. 스토어 생성 후 Redux DevTools 연결하기
+const store = createStore(rootReducer, composeWithDevTools());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      {/* 2. 리액트 컴포넌트에서 스토어를 사용할 수 있도록 App 컴포넌트를 react-redux에서 제공하는 Provider 컴포넌트로 감싸줌 - 이 컴포넌트 사용 시 store를 props 로 전달해줘야 함 */}
       <App />
     </Provider>
   </React.StrictMode>,
