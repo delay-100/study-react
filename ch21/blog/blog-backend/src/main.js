ad73924eac514428a4c62a53ba0f5069576871e2
delay100 <qor3127@naver.com>
2022-09-22 21:16:41 +0900

    허헣

---
+++ ch21/blog/blog-backend/src/main.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const Koa = require('koa')
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
+const mongoose = require('mongoose')
+
+const { PORT, MONGO_URI } = process.env
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((e) => {
+    console.error(e)
+  })
+
+const api = require('./api')
+
+const app = new Koa()
+const router = new Router()
+
+router.use('/api', api.routes())
+
+app.use(bodyParser())
+
+router.get('/', (ctx) => {
+  ctx.body = '홈'
+})
+
+router.get('/about', (ctx) => {
+  ctx.body = '소개'
+})
+
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params
+  ctx.body = name ? `${name}의 소개` : '소개'
+})
+
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+
+const port = PORT || 4000
+app.listen(port, () => {
+  console.log('Listening to port %d', port)
+})
