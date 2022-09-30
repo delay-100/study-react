2fa3f58a1cff44d04d46f74151caf1849a867111
delay100 <qor3127@naver.com>
2022-09-30 22:15:20 +0900

    postsCtrl.checkObjectId 주석해놓고 안지워서 에러가;

--- ch21/blog/blog-backend/src/api/posts/index.js
+++ ch21/blog/blog-backend/src/api/posts/index.js
@@ -6,16 +6,16 @@ const posts = new Router()
 posts.get('/', postsCtrl.list)
 posts.post('/', checkLoggedIn, postsCtrl.write)
 
-posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read)
-posts.delete('/:id', checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.remove)
-posts.patch('/:id', checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.update)
+// posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read)
+// posts.delete('/:id', checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.remove)
+// posts.patch('/:id', checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.update)
 
 // 라우터 분리하기
 const post = new Router() // /api/posts/:id
 
-post.get('/', postsCtrl.checkObjectId, postsCtrl.read)
-post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove)
-post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update)
+post.get('/', postsCtrl.read)
+post.delete('/', postsCtrl.checkOwnPost, postsCtrl.remove)
+post.patch('/', postsCtrl.checkOwnPost, postsCtrl.update)
 
 posts.use('/:id', postsCtrl.getPostById, post.routes())
 // posts.use('/:id', postsCtrl.checkObjectId, post.routes())
--- ch21/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ ch21/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -7,13 +7,14 @@ const { ObjectId } = mongoose.Types
 export const getPostById = async (ctx, next) => {
   const { id } = ctx.params
   if (!ObjectId.isValid(id)) {
-    ctx.status = 400
+    ctx.status = 400 // Bad Request
     return
   }
   try {
     const post = await Post.findById(id)
+    // 포스트가 존재하지 않을 때
     if (!post) {
-      ctx.status = 404
+      ctx.status = 404 // Not Found
       return
     }
     ctx.state.post = post
@@ -22,15 +23,6 @@ export const getPostById = async (ctx, next) => {
     ctx.throw(500, e)
   }
 }
-
-// export const checkObjectId = (ctx, next) => {
-//   const { id } = ctx.params
-//   if (!ObjectId.isValid(id)) {
-//     ctx.status = 400
-//     return
-//   }
-//   return next()
-// }
 
 export const checkOwnPost = (ctx, next) => {
   const { user, post } = ctx.state
@@ -41,16 +33,26 @@ export const checkOwnPost = (ctx, next) => {
   return next()
 }
 
+/*
+  POST /api/posts
+  {
+    title: '제목',
+    body: '내용',
+    tags: ['태그1', '태그2']
+  }
+*/
 export const write = async (ctx) => {
   const schema = Joi.object().keys({
-    title: Joi.string().required(),
-    body: Joi.string().required,
+    // 객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), // required() 가 있으면 필수 항목
+    body: Joi.string().required(),
     tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
   })
 
+  // 검증 후, 검증 실패시 에러처리
   const result = schema.validate(ctx.request.body)
   if (result.error) {
-    ctx.status = 400
+    ctx.status = 400 // Bad Request
     ctx.body = result.error
     return
   }
@@ -66,13 +68,16 @@ export const write = async (ctx) => {
     await post.save()
     ctx.body = post
   } catch (e) {
-    ctx.trhow(500, e)
+    ctx.throw(500, e)
   }
 }
 
+/*
+  GET /api/posts?username=&tag=&page=
+*/
 export const list = async (ctx) => {
-  // query는 문자열이기 때문에 숫자로 변환해줘야 함
-  // 값이 주어지지 않았다면 1을 기본으로 사용
+  // query 는 문자열이기 때문에 숫자로 변환해주어야합니다.
+  // 값이 주어지지 않았다면 1 을 기본으로 사용합니다.
   const page = parseInt(ctx.query.page || '1', 10)
 
   if (page < 1) {
@@ -86,6 +91,7 @@ export const list = async (ctx) => {
     ...(username ? { 'user.username': username } : {}),
     ...(tag ? { tags: tag } : {}),
   }
+
   try {
     const posts = await Post.find(query)
       .sort({ _id: -1 })
@@ -95,13 +101,6 @@ export const list = async (ctx) => {
       .exec()
     const postCount = await Post.countDocuments(query).exec()
     ctx.set('Last-Page', Math.ceil(postCount / 10))
-    // ctx.body = posts
-    //   .map((post) => post.toJSON())
-    //   .map((post) => ({
-    //     ...post,
-    //     body:
-    //       post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`, // body 길이가 200 이상이면 뒤에 '...'을 붙이고 문자열을 자르는 기능
-    //   }))
     ctx.body = posts.map((post) => ({
       ...post,
       body:
@@ -111,163 +110,63 @@ export const list = async (ctx) => {
     ctx.throw(500, e)
   }
 }
-// 특정 포스트 조회
+
+/*
+  GET /api/posts/:id
+*/
 export const read = async (ctx) => {
   ctx.body = ctx.state.post
-  // const { id } = ctx.params
-  // try {
-  //   const post = await Post.findById(id).exec()
-  //   if (!post) {
-  //     ctx.status = 404
-  //     return
-  //   }
-  //   ctx.body = post
-  // } catch (e) {
-  //   ctx.throw(500, e)
-  // }
 }
+
+/*
+  DELETE /api/posts/:id
+*/
 export const remove = async (ctx) => {
   const { id } = ctx.params
   try {
     await Post.findByIdAndRemove(id).exec()
-    ctx.status = 204
+    ctx.status = 204 // No Content (성공은 했지만 응답할 데이터는 없음)
   } catch (e) {
     ctx.throw(500, e)
   }
 }
+
+/*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정', '태그']
+  }
+*/
 export const update = async (ctx) => {
   const { id } = ctx.params
-
+  // write 에서 사용한 schema 와 비슷한데, required() 가 없습니다.
   const schema = Joi.object().keys({
     title: Joi.string(),
     body: Joi.string(),
-    tags: Joi.array().items(Joi.string()), // 문자열로 이루어진 배열
+    tags: Joi.array().items(Joi.string()),
   })
 
+  // 검증 후, 검증 실패시 에러처리
   const result = schema.validate(ctx.request.body)
   if (result.error) {
-    ctx.status = 400
+    ctx.status = 400 // Bad Request
     ctx.body = result.error
     return
   }
+
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
-      new: true, // 이 값 설정 시 업데이트 된 데이터를 반환
-      // false일 때는 업데이트 되기 전의 데이터를 반환
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+      // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
     }).exec()
     if (!post) {
       ctx.status = 404
       return
     }
+    ctx.body = post
   } catch (e) {
     ctx.throw(500, e)
   }
 }
-
-// let postId = 1
-
-// const posts = [
-//   {
-//     id: 1,
-//     title: '제목',
-//     body: '내용',
-//   },
-// ]
-
-// /* 포스트 작성
-// POST /api/posts
-// {title, body}
-// */
-
-// export const write = (ctx) => {
-//   const { title, body } = ctx.request.body
-//   postId += 1
-//   const post = { id: postId, title, body }
-//   posts.push(post)
-//   ctx.body = post
-// }
-
-// /* 포스트 목록 조회
-// GET /api/posts
-// */
-// export const list = (ctx) => {
-//   ctx.body = posts
-// }
-
-// /* 특정 포스트 조회
-// GET /api/posts/:id
-// */
-
-// export const read = (ctx) => {
-//   const { id } = ctx.params
-//   const post = posts.find((p) => p.id.toString() === id)
-//   if (!post) {
-//     ctx.status = 404
-//     ctx.body = {
-//       message: '포스트가 존재하지 않습니다.',
-//     }
-//     return
-//   }
-//   ctx.body = post
-// }
-
-// /* 특정 포스트 제거
-// DELETE /api/posts/:id
-//  */
-
-// export const remove = (ctx) => {
-//   const { id } = ctx.params
-//   const index = posts.findIndex((p) => p.id.toString() === id)
-//   if (index === -1) {
-//     ctx.status = 404
-//     ctx.body = {
-//       message: '포스트가 존재하지 않습니다',
-//     }
-//     return
-//   }
-//   posts.splice(index, 1)
-//   ctx.status = 204
-// }
-
-// /* 포스트 수정(교체)
-// PUT /api/posts/:id
-// {title. body}
-// */
-
-// export const replace = (ctx) => {
-//   const { id } = ctx.params
-//   const index = posts.findIndex((p) => p.id.toString() === id)
-//   if (index === -1) {
-//     ctx.status = 404
-//     ctx.body = {
-//       message: '포스트가 존재하지 않습니다',
-//     }
-//     return
-//   }
-//   posts[index] = {
-//     id,
-//     ...ctx.request.body,
-//   }
-//   ctx.body = posts[index]
-// }
-
-// /* 포스트 수정(특정 필드 변경)
-// PATCH /api/posts/:id
-// { title, body }
-// */
-// export const update = (ctx) => {
-//   const { id } = ctx.params
-//   const index = posts.findIndex((p) => p.id.toString() === id)
-//   if (index === -1) {
-//     ctx.status = 404
-//     ctx.body = {
-//       message: '포스트가 존재하지 않습니다',
-//     }
-//     return
-//   }
-//   posts[index] = {
-//     ...posts[index],
-//     ...ctx.request.body,
-//   }
-//   ctx.body = posts[index]
-// }
