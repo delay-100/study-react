864931bd9935fa7eea68fe07144369b4d4a0030e
delay100 <qor3127@naver.com>
2022-10-14 23:58:31 +0900

    오류나느중

--- ch23/blog/blog-frontend/src/App.js
+++ ch23/blog/blog-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route  } from 'react-router-dom'
 import PostListPage from './pages/PostListPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
@@ -7,6 +7,14 @@ import PostPage from './pages/PostPage'
 
 const App = () => {
   return (
+    <>
+    <Route component={PostListPage} path={['/@:username', '/']} exact />
+    <Route component={LoginPage} path="/login" />
+    <Route component={RegisterPage} path="/register" />
+    <Route component={WritePage} path="/write" />
+    <Route component={PostPage} path="/@:username/:postId" />
+    </>
+    /*
     <Routes>
       <Route path="/" element={<PostListPage />} />
       <Route path="/login" element={<LoginPage />} />
@@ -17,6 +25,7 @@ const App = () => {
         <Route path=":postId" element={<PostPage />} />
       </Route>
     </Routes>
+    */
   )
 }
 
--- ch23/blog/blog-frontend/src/pages/PostListPage.js
+++ ch23/blog/blog-frontend/src/pages/PostListPage.js
@@ -1,9 +1,12 @@
-import Button from '../components/common/Button'
+// import Button from '../components/common/Button' 
+import Header from '../components/common/Header'
 
 const PostListPage = () => {
   return (
     <div>
-      <Button>버튼</Button>
+      {/* <Button>버튼</Button> */}
+      <Header/>
+      <div>안녕하세요</div>
     </div>
   )
 }
